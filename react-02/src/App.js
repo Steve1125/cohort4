781e2fe34e58f52a0a2c16414665907a0050615b
Steve <niweiweish@gmail.com>
2020-06-14 22:38:58 -0600

    react-02 icon

--- react-02/src/App.js
+++ react-02/src/App.js
@@ -1,23 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+//import logo from './logo.svg';
+import earth from './earth.svg';
+import smile from './smile.svg';
+import sport from './sport.svg';
+import science from './science.svg';
+import dna from './dna.svg';
+import microbe from './microbe.svg';
+import brain from './brain-and-head.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="row ">
+          <div id="Science" className="span_4 column">
+            <img src={science} className="App-logo-reverse" alt="logo" />
+          </div>
+          <div id="Smile" className="span_4 column">
+            <img src={smile} className="App-logo" alt="logo" />
+          </div>
+          <div id="Sport" className="span_4 column">
+            <img src={sport} className="App-logo" alt="logo" />
+          </div>
+          <div id="DNA" className="span_4 column">
+            <img src={dna} className="App-logo" alt="logo" />
+          </div>
+          <div id="Earth" className="span_4 column">
+            <img src={earth} className="App-logo" alt="logo" />
+          </div>
+          <div id="Microbe" className="span_4 column">
+            <img src={microbe} className="App-logo" alt="logo" />
+          </div>
+          <div id="Brain" className="span_4 column">
+            <img src={brain} className="App-logo" alt="logo" />
+          </div>
+        </div>
+
+
       </header>
     </div>
   );
